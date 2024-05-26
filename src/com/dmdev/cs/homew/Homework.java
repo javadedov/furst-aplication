b9297a2899b22ee8c250250a5c6c05476031691e
Nikita <nikitasuper472@gmail.com>
2024-05-26 21:54:23 +0300

    готовое домашнее задание1

---
+++ src/com/dmdev/cs/homew/Homework.java
@@ -0,0 +1,138 @@
+package com.dmdev.cs.homew;
+
+public class Homework {
+   private int i =10;
+   private  Integer age = 10;
+   public int getI() {
+      return i;
+   }
+   public void setI(int i) {
+      this.i = i;
+   }
+   public Integer getAge() {
+      return age;
+   }
+   public void setAge(Integer age) {
+      this.age = age;
+   }
+
+   private byte b =113;
+   private Byte number = 113;
+   public byte getB() {
+      return b;
+   }
+   public void setB(byte b) {
+      this.b = b;
+   }
+   public Byte getNumber() {
+      return number;
+   }
+   public void setNumber(Byte number) {
+      this.number = number;
+   }
+
+   private short s = 32700;
+   public short getS() {
+      return s;
+   }
+   public void setS(short s) {
+      this.s = s;
+   }
+
+   private Short number1 = 32700;
+   public Short getNumber1() {
+      return number1;
+   }
+   public void setNumber1(Short number1) {
+      this.number1 = number1;
+   }
+
+   private long l = 10000;
+   public long getL() {
+      return l;
+   }
+   public void setL(long l) {
+      this.l = l;
+   }
+   private Long number2 = 10000L;
+   public Long getNumber2() {
+      return number2;
+   }
+   public void setNumber2(Long number2) {
+      this.number2 = number2;
+   }
+
+   private float f = 10.1f;
+   public float getF() {
+      return f;
+   }
+   public void setF(float f) {
+      this.f = f;
+   }
+   private Float number3 = 10.1f;
+   public Float getNumber3() {
+      return number3;
+   }
+   public void setNumber3(Float number3) {
+      this.number3 = number3;
+   }
+
+   private double d = 10.1d;
+   public double getD() {
+      return d;
+   }
+   public void setD(double d) {
+      this.d = d;
+   }
+   private Double number4 = 10.1d;
+   public Double getNumber4() {
+      return number4;
+   }
+   public void setNumber4(Double number4) {
+      this.number4 = number4;
+   }
+
+   private char ch = 'A';
+   public char getCh() {
+      return ch;
+   }
+   public void setCh(char ch) {
+      this.ch = ch;
+   }
+   private Character ch1 = 'A';
+   public Character getCh1() {
+      return ch1;
+   }
+   public void setCh1(Character ch1) {
+      this.ch1 = ch1;
+   }
+
+   private boolean b1  = true;
+   public boolean getB1() {
+      return b1;
+   }
+   private Boolean b2 = true;
+   public Boolean getB2() {
+      return b2;
+   }
+   public void setB2(Boolean b2) {
+      this.b2 = b2;
+   }
+
+   private String sts =(" Я стану ппрограмисстом ");
+   public String getSts() {
+      return sts;
+   }
+   public void setSts(String sts) {
+      this.sts = sts;
+   }
+
+   }
+
+
+
+
+
+
+
+
---
+++ src/com/dmdev/cs/homew/Homework1.java
@@ -0,0 +1,32 @@
+package com.dmdev.cs.homew;
+
+
+public class Homework1 {
+
+    String name;
+    int age;
+    Float weight;
+    boolean bool ;
+    Integer height;
+
+    public static void main(String[] args) {
+
+        String name =("Привет , меня зовут Никита ");
+        int age =19;
+        Float weight =75.5f;
+        Boolean bool =true;
+        Integer height = 194;
+
+
+        System.out.println(name);
+        System.out.println(weight);
+        System.out.println(bool);
+        System.out.println(height);
+        System.out.println(age);
+
+
+
+
+
+    }
+}
